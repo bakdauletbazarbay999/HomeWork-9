a42bcf4bc5ea0001c3c7eabdca77d8eef0a5ad33
bakdauletbazarbay999 <bakdauletbazarbay9@gmail.com>
2025-11-05 14:27:41 +0500

    Add files via upload

---
+++ Composite.cpp
@@ -0,0 +1,90 @@
+﻿// Composite
+#include <iostream>
+#include <vector>
+#include <string>
+#include <memory>
+using namespace std;
+
+class FileSystemComponent {
+public:
+    virtual void Display(string indent = "") const = 0;
+    virtual int GetSize() const = 0;
+    virtual string GetName() const = 0;
+    virtual ~FileSystemComponent() {}
+};
+class File : public FileSystemComponent {
+    string name;
+    int size;
+public:
+    File(const string& n, int s) : name(n), size(s) {}
+    void Display(string indent = "") const override {
+        cout << indent << "- File: " << name << " (" << size << " KB)\n";
+    }
+    int GetSize() const override { return size; }
+    string GetName() const override { return name; }
+};
+class Directory : public FileSystemComponent {
+    string name;
+    vector<shared_ptr<FileSystemComponent>> components;
+public:
+    Directory(const string& n) : name(n) {}
+
+    void Add(shared_ptr<FileSystemComponent> comp) {
+        for (auto& c : components)
+            if (c->GetName() == comp->GetName()) {
+                cout << "Component '" << comp->GetName() << "' already exists in " << name << endl;
+                return;
+            }
+        components.push_back(comp);
+    }
+
+    void Remove(const string& compName) {
+        for (auto it = components.begin(); it != components.end(); ++it) {
+            if ((*it)->GetName() == compName) {
+                components.erase(it);
+                cout << "Removed '" << compName << "' from " << name << endl;
+                return;
+            }
+        }
+        cout << "Component '" << compName << "' not found in " << name << endl;
+    }
+    void Display(string indent = "") const override {
+        cout << indent << "+ Directory: " << name << "\n";
+        for (auto& c : components)
+            c->Display(indent + "   ");
+    }
+    int GetSize() const override {
+        int total = 0;
+        for (auto& c : components)
+            total += c->GetSize();
+        return total;
+    }
+    string GetName() const override { return name; }
+};
+int main() {
+    auto root = make_shared<Directory>("Root");
+    auto docs = make_shared<Directory>("Documents");
+    auto pics = make_shared<Directory>("Pictures");
+
+    auto file1 = make_shared<File>("Resume.docx", 120);
+    auto file2 = make_shared<File>("Budget.xlsx", 200);
+    auto file3 = make_shared<File>("photo1.jpg", 350);
+    auto file4 = make_shared<File>("photo2.jpg", 500);
+
+    docs->Add(file1);
+    docs->Add(file2);
+    pics->Add(file3);
+    pics->Add(file4);
+
+    root->Add(docs);
+    root->Add(pics);
+
+    root->Display();
+    cout << "\nTotal size of Root: " << root->GetSize() << " KB\n";
+
+    docs->Remove("Resume.docx");
+    root->Display();
+    cout << "\nUpdated total size: " << root->GetSize() << " KB\n";
+
+    return 0;
+}
---
+++ Facade.cpp
@@ -0,0 +1,105 @@
+﻿// Facade
+#include <iostream>
+#include <string>
+using namespace std;
+
+class TV {
+public:
+    void on() { cout << "TV is ON\n"; }
+    void off() { cout << "TV is OFF\n"; }
+    void setChannel(int channel) { cout << "TV channel set to " << channel << "\n"; }
+};
+class AudioSystem {
+    int volume = 10;
+public:
+    void on() { cout << "AudioSystem is ON\n"; }
+    void off() { cout << "AudioSystem is OFF\n"; }
+    void setVolume(int v) {
+        volume = v;
+        cout << "Volume set to " << volume << "\n";
+    }
+};
+class DVDPlayer {
+public:
+    void on() { cout << "DVD Player is ON\n"; }
+    void off() { cout << "DVD Player is OFF\n"; }
+    void play() { cout << "DVD playing...\n"; }
+    void pause() { cout << "DVD paused.\n"; }
+    void stop() { cout << "DVD stopped.\n"; }
+};
+class GameConsole {
+public:
+    void on() { cout << "Game Console is ON\n"; }
+    void startGame(const string& game) { cout << "Starting game: " << game << "\n"; }
+};
+class HomeTheaterFacade {
+    TV* tv;
+    AudioSystem* audio;
+    DVDPlayer* dvd;
+    GameConsole* console;
+public:
+    HomeTheaterFacade(TV* t, AudioSystem* a, DVDPlayer* d, GameConsole* c)
+        : tv(t), audio(a), dvd(d), console(c) {
+    }
+    void watchMovie() {
+        cout << "\n=== Starting Movie Mode ===\n";
+        tv->on();
+        audio->on();
+        audio->setVolume(15);
+        dvd->on();
+        dvd->play();
+        cout << "Enjoy your movie!\n";
+    }
+    void endMovie() {
+        cout << "\n=== Shutting Down Movie Mode ===\n";
+        dvd->stop();
+        dvd->off();
+        audio->off();
+        tv->off();
+    }
+    void playGame(const string& game) {
+        cout << "\n=== Starting Game Mode ===\n";
+        tv->on();
+        audio->on();
+        audio->setVolume(12);
+        console->on();
+        console->startGame(game);
+    }
+    void listenToMusic() {
+        cout << "\n=== Music Mode ===\n";
+        tv->on();
+        audio->on();
+        audio->setVolume(18);
+        cout << "TV set to Audio Input. Playing music...\n";
+    }
+    void setVolume(int volume) {
+        audio->setVolume(volume);
+    }
+    void shutdownAll() {
+        cout << "\n=== Shutting Down All Systems ===\n";
+        tv->off();
+        audio->off();
+        dvd->off();
+        cout << "All systems are now OFF.\n";
+    }
+};
+int main() {
+    TV tv;
+    AudioSystem audio;
+    DVDPlayer dvd;
+    GameConsole console;
+
+    HomeTheaterFacade home(&tv, &audio, &dvd, &console);
+
+    home.watchMovie();
+    home.setVolume(20);
+    home.endMovie();
+
+    home.playGame("The Witcher 3");
+    home.shutdownAll();
+
+    home.listenToMusic();
+    home.shutdownAll();
+
+    return 0;
+}
